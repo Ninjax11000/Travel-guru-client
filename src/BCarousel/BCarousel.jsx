c7a694e77cbddb52ed7dad655109201f79cabd06
Ninjax11000 <jamiljoy007@gmail.com>
2023-12-07 21:26:06 +0600

    updated slider layout

--- src/BCarousel/BCarousel.jsx
+++ src/BCarousel/BCarousel.jsx
@@ -1,6 +1,9 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext} from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import sajek from '/images/places/Sajek.png'
+import sajek from '/images/places/Sajek.png';
+import sreemongol from '/images/places/Sreemongol.png';
+import sundorbon from '/images/places/sundorbon.png';
+import coxsbazar from '/images/places/coxsbazar.png';
 
 // Import Swiper styles
 import 'swiper/css';
@@ -11,18 +14,24 @@ import '../styles.css';
 import { Pagination, Navigation } from 'swiper/modules';
 
 import { imageContext } from '../Layout/Main';
+import { Link } from 'react-router-dom';
+
+
 
 const BCarousel = () => {
     const { setText, setUrl } = useContext(imageContext);
-    const handletext = () => {
-        setText('new TExt');
+
+    const textCoxs = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima explicabo consectetur corrupti beatae corporis quaerat tenetur sint ipsam sequi nobis!";
+    const textSajek = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima explicabo consectetur corrupti beatae corporis quaerat tenetur sint ipsam sequi nobis!";
+    const textSreemongol = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima explicabo consectetur corrupti beatae corporis quaerat tenetur sint ipsam sequi nobis!";
+    const textSundorbon = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima explicabo consectetur corrupti beatae corporis quaerat tenetur sint ipsam sequi nobis!";
+    
+    const handleTextUrl = (text, uRl)=>{
+            setText(text);
+            setUrl(uRl);
     }
 
-    const handleurl = () => {
-        setUrl('new URL')
-    }
-
-
+    
 
     return (
         <div >
@@ -33,25 +42,44 @@ const BCarousel = () => {
                     centeredSlides={true}
                     spaceBetween={30}
                     pagination={{
-                        type: 'fraction',
+                        dynamicBullets: true,
+                        
                     }}
                     navigation={true}
                     modules={[Pagination, Navigation]}
                     className="mySwiper"
+                    
                 >
-                    <SwiperSlide style={{backgroundImage:`url(${sajek})`,backgroundSize:'100% 100%'}} >
-                        
+                    <SwiperSlide 
+                    style={{backgroundImage:`url(${coxsbazar})`,backgroundSize:'100% 100%',cursor: 'pointer'}} 
+                    onClick={()=>handleTextUrl(textCoxs, coxsbazar)}
+                    >  
                     </SwiperSlide>
-                    <SwiperSlide>Slide 2</SwiperSlide>
-                    <SwiperSlide>Slide 3</SwiperSlide>
-                    <SwiperSlide>Slide 4</SwiperSlide>
+                    <SwiperSlide 
+                    style={{backgroundImage:`url(${sajek})`,backgroundSize:'100% 100%',cursor: 'pointer'}}
+                    onClick={()=>handleTextUrl(textSajek, sajek)}
+                    >   
+                    </SwiperSlide>
+                    <SwiperSlide 
+                    style={{backgroundImage:`url(${sreemongol})`,backgroundSize:'100% 100%',cursor: 'pointer'}} 
+                    onClick={()=>handleTextUrl(textSreemongol, sreemongol)}
+                    >   
+                    </SwiperSlide>
+                    <SwiperSlide 
+                    style={{backgroundImage:`url(${sundorbon})`,backgroundSize:'100% 100%',cursor: 'pointer'}} 
+                    onClick={()=>handleTextUrl(textSundorbon, sundorbon)}
+                    >   
+                    </SwiperSlide>
+                   
+                    
                 </Swiper>
-
-
+        
+                
             </>
+            
             <h2>Carousel Comming soon!!!!</h2>
-            <button onClick={handletext} className='btn btn-warning'></button>
-            <button onClick={handleurl} className='btn btn-primary'></button>
+            {/* <button onClick={handletext} className='btn btn-warning'></button>
+            <button onClick={handleurl} className='btn btn-primary'></button> */}
         </div>
     );
 };
